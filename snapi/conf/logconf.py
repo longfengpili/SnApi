033d97283f3e18e9b3ccd4e5743ecf75ba80b962
longfengpili <398745129@qq.com>
2023-07-27 14:25:32 +0800

    [优化]docker-compose network

--- snapi/conf/logconf.py
+++ snapi/conf/logconf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: longfengpili
 # @Date:   2023-07-17 18:04:38
-# @Last Modified by:   chunyang.xu
-# @Last Modified time: 2023-07-26 19:02:02
+# @Last Modified by:   longfengpili
+# @Last Modified time: 2023-07-27 13:44:15
 
 
 import re
@@ -13,9 +13,10 @@ import colorlog
 
 # logging settings
 USERPATH = os.environ['USERPROFILE'] if 'USERPROFILE' in os.environ else os.environ['HOME'] if 'HOME' in os.environ else ''
-LOG_BASE_PATH = os.path.join(USERPATH, '.snapilog')  # 可以user目录下查看日志
+LOG_BASE_PATH = os.path.join(USERPATH, 'snapilog')  # 可以user目录下查看日志
 PROJECT_NAME = re.sub(':?\\\\', '_', os.getcwd())
 PROJECT_NAME = PROJECT_NAME[1:] if PROJECT_NAME.startswith('/') else PROJECT_NAME  # linux
+
 LOGGING_CONFIG = {
     'version': 1,  # 保留字
     'disable_existing_loggers': False,  # 禁用已经存在的logger实例
